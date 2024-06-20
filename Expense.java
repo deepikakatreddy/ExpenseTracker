1e762f6567417ebfb99b60d81ceacee8ea8f2e34
Deepika katreddy <131348836+deepikakatreddy@users.noreply.github.com>
2024-06-20 18:45:33 +0530

    Expense.java

---
+++ Expense.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Expense {
+    String category;
+    double amount;
+    String date;
+
+    Expense(String category, double amount, String date) {
+        this.category = category;
+        this.amount = amount;
+        this.date = date;
+    }
+}
